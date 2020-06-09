c9bc8ddabfafa409561ff72ecd2fbaaf78afcd3b
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-06-09 20:32:34 -0300

    adicionando material-ui e ajustes menores

--- frontend/src/pages/NotFound/index.js
+++ frontend/src/pages/NotFound/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./styles.css";
 
-import logoImg from './img/robot.svg';
+import logoImg from "../../assets/img/robot.svg";
 
 export default function NotFound() {
   return (
     <div class="container">
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Nunito+Sans:wght@300;400;600;700&display=swap" rel="stylesheet"></link>
-    <div class="not"> 
-        <h1> 
-            <strong class="s404">404</strong> 
-            <span class="snot">Pagina não encontrada </span>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Nunito+Sans:wght@300;400;600;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div class="not">
+        <h1>
+          <strong class="s404">404</strong>
+          <span class="snot">Pagina não encontrada </span>
         </h1>
-    </div>
-    <div class="cimg">
+      </div>
+      <div class="cimg">
         <img src={logoImg} alt="robot" />
+      </div>
     </div>
-</div>
   );
 }
