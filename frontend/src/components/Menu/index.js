a669f530bf1ba8fe1314f48fd62545b270fbe94e
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-06-10 21:18:29 -0300

    Sidebar de categorias

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -1,24 +1,70 @@
-import React from "react";
-import { MdMenu, MdShoppingBasket } from "react-icons/md";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { MdMenu, MdShoppingBasket, MdKeyboardArrowRight } from "react-icons/md";
 import { FiLogIn } from "react-icons/fi";
+import api from "../../services/Api";
 
 import "./styles.css";
+
+import M from "materialize-css/dist/js/materialize.min.js";
+import "materialize-css/dist/css/materialize.min.css";
+
+const SideBar = () => {
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    async function Data() {
+      const elem = document.querySelector(".sidenav");
+      const instance = M.Sidenav.init(elem, {
+        edge: "left",
+        inDuration: 250,
+      });
+      const { data } = await api.get("/categorias");
+      setCategorias(data);
+    }
+    Data();
+  }, []);
+
+  return (
+    <div>
+      <ul id="slide-out" className="sidenav">
+        <li className="header-categorias">
+          <h2>Categorias</h2>
+        </li>
+        <li className="btn-voltar-categorias">
+          <Link class="sidenav-close">
+            <span className="txt-voltar-categorias">
+              Voltar
+              <MdKeyboardArrowRight />
+            </span>
+          </Link>
+        </li>
+        {categorias.map((categoria) => (
+          <li key={categoria.id} className="item-categoria">
+            <Link className="waves-effect">{categoria.nome}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default function Menu() {
   return (
     <>
       <nav className="nav-menu">
-        <div class="nav-wrapper">
+        <div className="nav-wrapper">
           <ul id="nav-mobile" class="left hide-on-med-and-down">
             <li>
-              <div className="btn-menu-categorias">
+              <div
+                className="sidenav-trigger btn-menu-categorias"
+                data-target="slide-out"
+              >
                 <MdMenu />
               </div>
             </li>
           </ul>
-          <a href="#!" class="brand-logo">
-            Logo
-          </a>
+          <div className="brand-logo">Logo</div>
 
           <ul class="right hide-on-med-and-down">
             <li>
@@ -27,14 +73,17 @@ export default function Menu() {
               </div>
             </li>
             <li>
-              <a class="waves-effect waves-light btn btn-entrar">
-                <FiLogIn className="icon-entrar" />
-                Entrar
-              </a>
+              <Link class="waves-effect waves-light btn btn-entrar">
+                <span className="txt-btn-entrar">
+                  <FiLogIn className="icon-entrar" />
+                  Entrar
+                </span>
+              </Link>
             </li>
           </ul>
         </div>
       </nav>
+      <SideBar />
     </>
   );
 }
