2f6ea94904c4cbcc2dab54651e0542660495f1eb
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-06-11 23:12:58 -0300

    Pagina de home basica

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,8 +1,35 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from "../../services/Api";
 import "./styles.css";
 
+import Card from "../../components/Card";
+
 function Main() {
-  return <div class="container-main"></div>;
+  const [comercios, setComercios] = useState([]);
+
+  useEffect(async () => {
+    const { data } = await api.get("/comercios");
+    setComercios(data);
+  }, []);
+
+  return (
+    <>
+      <div className="container-main">
+        <h1 className="titulo-main">
+          Encontre um comercio proximo de você e ajude os comerciantes locais
+        </h1>
+        <cite className="referencia-foto">Photo by nrd on Unsplash</cite>
+      </div>
+      <div className="container-comercios">
+        {comercios.map((comercio) => (
+          <Card
+            titulo={comercio.nome}
+            url={comercio.url}
+            idComercio={comercio.id}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 export default Main;
