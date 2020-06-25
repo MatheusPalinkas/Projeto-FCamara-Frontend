9cc43c1adb963135feff3ffe34c0e6f6a20a73bc
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-06-24 23:55:14 -0300

    Adicionando redux ao porjeto

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //import pages
@@ -14,50 +15,51 @@ import MinhaConta from "./pages/MinhaConta";
 
 import Menu from "./components/Menu";
 
+import store from "./store";
+
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Menu
-        user={{
-          idComercio: 2,
-          id: 2,
-          url:
-            "https://static1.purepeople.com.br/articles/7/28/80/37/@/3267022-larissa-manoela-chamou-atencao-dos-segui-624x600-2.jpg",
-        }}
-      />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/home/:idCategoria" component={Main} />
+      <Provider store={store}>
+        <Menu />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/home/:idCategoria" component={Main} />
 
-        <Route exact path="/cadastro" component={Cadastro} />
+          <Route exact path="/cadastro" component={Cadastro} />
 
-        <Route
-          exact
-          path="/produto/vendedor/:idComercio"
-          component={ProdutosVendedor}
-        />
-        <Route
-          exact
-          path="/comercio/:idComercio"
-          component={ProdutosComercio}
-        />
-        <Route exact path="/novo/produto/:idComercio" component={NovoProduto} />
+          <Route
+            exact
+            path="/produto/vendedor/:idComercio"
+            component={ProdutosVendedor}
+          />
+          <Route
+            exact
+            path="/comercio/:idComercio"
+            component={ProdutosComercio}
+          />
+          <Route
+            exact
+            path="/novo/produto/:idComercio"
+            component={NovoProduto}
+          />
 
-        <Route
-          exact
-          path="/pedidos/vendedor/:idComercio"
-          component={PedidosVendedor}
-        />
+          <Route
+            exact
+            path="/pedidos/vendedor/:idComercio"
+            component={PedidosVendedor}
+          />
 
-        <Route
-          exact
-          path="/dados/pedido/:idComercio"
-          component={DetalhesPedido}
-        />
+          <Route
+            exact
+            path="/dados/pedido/:idComercio"
+            component={DetalhesPedido}
+          />
 
-        <Route exact path="/editar/conta" component={MinhaConta} />
-        <Route exact path="*" component={NotFound} />
-      </Switch>
+          <Route exact path="/editar/conta" component={MinhaConta} />
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      </Provider>
     </BrowserRouter>
   );
 }
